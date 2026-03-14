33bf7a95b43dc37087c8e738de012ff4ef348a2c
Brielle Thames <dragongirl8@yahoo.com>
2026-03-13 21:53:12 -0400

    Update lab_1a.py

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,18 @@ def main():
     name = "Brielle Thames" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
-
+    print("Hello, my name is Brielle Thames from Barbara " \
+    "Ingram School for the Arts, a magnet high school program. " \
+    "I, as an enthusiastic game designer, animator, visual artist," \
+    "and computer science explorer enjoy integrating my artistic abilities" \
+    "and computer knowledge. I usually don't take the typical or easiest " \
+    "route to get to a destination, but this is to ensure I have the most " \
+    "success and the subject is explored to my fullest potential. " \
+    "I love impulsively beginning new projects, but unlike typical impulsivity," \
+    " I complete the tasks I give myself. A prime example is my video game, " \
+    "Rogue Planet which is advertised on my YouTube channel, which is planned to " \
+    "release during September of this year. I have plenty of interests concentrated "
+    "in Computer Science and Arts, although my ultimate career goal is to integrate " \
+    "main components of computer science with secondary artistic influences. ")
 if __name__ == "__main__":
     main()
